62604a212addd7e45fcc17fbb86933282f953ad3
Donnie Santos <DonaldJSantos@Gmail.com>
2015-09-19 18:21:26 -0400

    Section 3.

--- Section3/Example2_Constructors/Program.cs
+++ Section3/Example2_Constructors/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Example2_Constructors
+﻿namespace Example2_Constructors
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Person donnie = new Person("Donnie", "Santos", 37);
+            Person michael = new Person("Michael", "Santos", 29);
+
+            Person alana = new Person();
+            alana.FirstName = "Alana";
+            alana.LastName = "Santos";
+            alana.Age = 43;
+
+            int sum1 = donnie.AddTwoIntegers(7, 8);
+            int sum2 = donnie.AddTwoIntegers(1, 9);
+            int sum3 = donnie.AddTwoIntegers(2, 13);
+
+            donnie.Speak("I am the smartest person in the world!");
+            michael.Speak("That is definitely true.");
+            alana.Speak("Word.");
+
+            System.Console.ReadKey();
         }
     }
 }
