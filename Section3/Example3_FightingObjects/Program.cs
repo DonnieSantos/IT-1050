41a140e8bdb4689cf7107f1817d4726f1e8bfb1f
Donnie Santos <DonaldJSantos@Gmail.com>
2015-09-19 20:05:01 -0400

    Updates.

---
+++ Section3/Example3_FightingObjects/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Example3_FightingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Show.HorizontalDivider();
+
+            GoodGuy harry = new GoodGuy("Harry Potter", 999);
+            GoodGuy hermione = new GoodGuy("Hermione Granger", 200);
+            GoodGuy ron = new GoodGuy("Ron Weasley", 100);
+
+            BadGuy voldemort = new BadGuy("Tom Riddle", 800);
+            BadGuy bellatrix = new BadGuy("Bellatrix Lestrange", 600);
+            BadGuy draco = new BadGuy("Draco Malfoy", 400);
+
+            Show.HorizontalDivider();
+
+            voldemort.Steal(harry, 200);
+            voldemort.Steal(draco, 200);
+            bellatrix.Steal(draco, 200);
+
+            harry.Capture(bellatrix);
+            harry.Give(hermione, 400);
+            harry.Give(ron, 400);
+
+            Show.HorizontalDivider();
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Section3/Example3_FightingObjects/Show.cs
@@ -0,0 +1,73 @@
+﻿namespace Example3_FightingObjects
+{
+    public static class Show
+    {
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
+        public static void HorizontalDivider()
+        {
+            System.Console.WriteLine(" +========================================================+");
+        }
+
+        public static void CaptureMessage(GoodGuy goodGuy, BadGuy badGuy, double amount)
+        {
+            Show.Formatted(goodGuy.Name + " captured " + badGuy.Name + " and got " + amount);
+            Show.Money(goodGuy);
+            Show.Money(badGuy);
+        }
+
+        public static void GiveMessage(GoodGuy giver, GoodGuy receiver, double amount)
+        {
+            Show.Formatted(giver.Name + " gave " + amount + " to " + receiver.Name);
+            Show.Money(giver);
+            Show.Money(receiver);
+        }
+
+        public static void StealMessage(BadGuy badGuy, GoodGuy goodGuy, double amount)
+        {
+            Show.Formatted(badGuy.Name + " stole " + amount + " from " + goodGuy.Name);
+            Show.Money(badGuy);
+            Show.Money(goodGuy);
+        }
+
+        public static void StealMessage(BadGuy badGuy, BadGuy otherBadGuy, double amount)
+        {
+            Show.Formatted(badGuy.Name + " stole " + amount + " from " + otherBadGuy.Name);
+            Show.Money(badGuy);
+            Show.Money(otherBadGuy);
+        }
+
+        public static void CreationMessage(string name, double amount)
+        {
+            Show.Formatted(name + " has been created and has " + amount);
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
+        private static void Money(GoodGuy goodGuy)
+        {
+            Show.Money(goodGuy.Name, goodGuy.Money);
+        }
+
+        private static void Money(BadGuy badGuy)
+        {
+            Show.Money(badGuy.Name, badGuy.Money);
+        }
+
+        private static void Money(string name, double amount)
+        {
+            Show.Formatted(name + " now has a total of " + amount);
+        }
+
+        private static void Formatted(string output)
+        {
+            output += ".";
+            System.Console.WriteLine(" | " + output.PadRight(54) + " | ");
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////
+    }
+}
