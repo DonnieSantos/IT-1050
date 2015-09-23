3bdf93adbdcf36b78b9a493e702897f9a49f8908
Donnie Santos <DonaldJSantos@Gmail.com>
2015-09-22 20:25:03 -0400

    Added Two Files

---
+++ Temp/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApplication2
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public Person Spouse;
+        public bool IsMarried()
+        {
+            return Spouse != null;
+        }
+    }
+}
---
+++ Temp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person();
+            Person p3 = new Person();
+
+            System.Console.WriteLine("Are you married? (y/n) ");
+
+            if (System.Console.ReadLine().ToLower().StartsWith("y"))
+            {
+                p1.Spouse = new Person();
+                p1.Spouse.Name = "";
+            }
+        }
+    }
+}
