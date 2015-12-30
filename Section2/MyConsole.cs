2f93a07eeaba0b0c85ac165a38c941ee31adb828
Donald J. Santos <DonaldJSantos@Gmail.com>
2015-12-30 18:46:11 -0500

    Updates.
    Updates.

---
+++ Section2/MyConsole.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApplication1
+{
+    public class MyConsole
+    {
+        public static string ReadString(string prompt)
+        {
+            System.Console.Write(" " + prompt.PadRight(33) + " : ");
+            return System.Console.ReadLine();
+        }
+
+        public static int ReadInteger(string prompt)
+        {
+            return int.Parse(ReadString(prompt));
+        }
+
+        public static bool ReadBool(string prompt)
+        {
+            string input = System.Console.ReadLine().ToLower();
+            return input.StartsWith("y") || input.StartsWith("t");
+        }
+    }
+}
